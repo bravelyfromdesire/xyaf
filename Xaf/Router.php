d318e624d9413b7840d2b4900cdb1c3970f36a63
x <bravelyfromdesire@gmail.com>
2018-08-05 10:56:53 +0800

    实现路由器

--- Xaf/Router.php
+++ Xaf/Router.php
@@ -10,14 +10,34 @@ class Router
     }
 
     /**
-     * 解析请求，然后根据配置找到路由到合适的控制器、方法
+     * 解析请求，然后根据配置找到合适的控制器、方法
+     * {host}/controller
+     * 支持GET、POST、PUT、DELETE
      * @param void
      * @return array e.g.[$controller, $action]
      */
     public function route()
     {
+        //Default
         $controller = 'Index';
-        $action = 'Index';
+        $action = 'get';
+        $method = 'get';
+
+        //Controller & Action
+        $requestUri = $_SERVER['REQUEST_URI'];
+        $what = explode('.', $requestUri);
+        if ($what) {
+            $what = explode('/', trim($what[0], '/'));
+            if ($what) {
+                $controller = ucfirst($what[0]);
+                $method = strtolower($_SERVER['REQUEST_METHOD']);
+                if (isset($what[1])) {
+                    $action = $method . ucfirst($what[1]);
+                } else {
+                    $action = $method . $controller;
+                }
+            }
+        }
 
         return [$controller, $action];
     }
