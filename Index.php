0a07306d679cd4e6ba1b438697ce2124e30a8d79
x <bravelyfromdesire@gmail.com>
2018-08-05 10:54:59 +0800

    index.php重命名为Index.php

--- Index.php
+++ Index.php
@@ -1,9 +1,13 @@
 <?php
 error_reporting(E_ALL);
+require_once('./Autoload.php');
 //配置在哪？
 define('APP_PATH', __DIR__);
 define('CONFIG_PATH', APP_PATH . '/config/');
 define('APP_CONFIG', CONFIG_PATH . 'app.ini');
+
+use Xaf\Application;
+
 //调用函数
 try {
     /**
@@ -12,15 +16,7 @@ try {
      * @param string section 如果config是ini文件，则section指代特定的配置节
      * @return Yaf_Application
      */
-    $app = new Yaf\Application(APP_CONFIG);
-
-    /**
-     * Yaf_Application bootstrap()
-     * 启动程序之前的初始化：寻找Bootstrap.php文件，并执行其中所有的以_init开头的方法
-     * @param void
-     * @return Yaf\Application
-     */
-    $app = $app->bootstrap();
+    $app = new Application(APP_CONFIG);
 
     /**
      * Yaf_Application run()
@@ -29,6 +25,7 @@ try {
      * @return boolean
      */
     $app->run();
+
 } catch (Exception $exception) {
     var_dump($exception);die;
 } catch (Error $error) {
